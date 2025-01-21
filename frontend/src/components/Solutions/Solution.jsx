9b0a943156fa23dc090374a9b53fcb57bcdfdb70
alisj3 <alisher123841@bk.ru>
2025-01-21 18:52:36 +0500

    Minor fixes

--- frontend/src/components/Solutions/Solution.jsx
+++ frontend/src/components/Solutions/Solution.jsx
@@ -2,15 +2,15 @@ export function Solutions(){
     return (
         <>
             <div className='intro-2'>
-                <div>
-                    <img style={{width: "80%"}} src="/Doc.png" alt="" />
+                <div style={{width: "50%", display: "flex", flexDirection: "column", alignItems: "center"}}>
+                    <img style={{width: "40%"}} src="/Doc.png" alt="" />
                     <h4 style={{paddingTop: "40px", paddingBottom: "40px", fontSize: "2.2em"}}>Прототип созданный ИИ</h4>
                 </div>
                 <hr />
-                <div style={{width: "40%",  textAlign: "left"}}>
-                    <h3 style={{width: "100%",  textAlign: "left", fontSize: "3em"}}>ДОК - СТАНЦИЯ</h3>
-                    <h4 style={{paddingTop: "40px", paddingBottom: "40px", fontSize: "2.2em"}}>ПОЛЬЗА И ФУНКЦИОНАЛ</h4>
-                    <p style={{fontSize: "1.5em", fontWeight: "600"}}>Представляем нашу док-станцию с интегрированной нейросетью, созданную для вашего удобства. Она помогает слушать музыку, общаться, управлять умным домом и выполнять повседневные задачи проще и быстрее.</p>
+                <div style={{width: "50%",  textAlign: "left", paddingLeft: "40px"}}>
+                    <h3 style={{width: "100%",  textAlign: "left", fontSize: "2em"}}>ДОК - СТАНЦИЯ</h3>
+                    <h4 style={{paddingTop: "40px", paddingBottom: "40px", fontSize: "1.5em"}}>ПОЛЬЗА И ФУНКЦИОНАЛ</h4>
+                    <p style={{fontSize: "1em", fontWeight: "600", width: "70%"}}>Представляем нашу док-станцию с интегрированной нейросетью, созданную для вашего удобства. Она помогает слушать музыку, общаться, управлять умным домом и выполнять повседневные задачи проще и быстрее.</p>
                 </div>
             </div>
         </>
